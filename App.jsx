f2d7e240a3e6d16676e12b5430ca50a8c6a56c6b
Jimenac5 <Jimenacerda03@gmail.com>
2025-05-09 23:11:48 -0400

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import Weather from './components/Weather'
+import React, {useEffect} from'react';
+
+
+const App = () => {
+
+
+return (
+    <div className='app'>
+      <Weather/>
+    </div>
+  )
+}
+export default App
